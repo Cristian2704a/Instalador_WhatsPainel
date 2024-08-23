268ab7d73f1688d874db08af655f4b5bdc82790f
Cristian2704a <61756734+Cristian2704a@users.noreply.github.com>
2024-08-23 10:28:25 -0300

    Update _banner.sh

--- utils/_banner.sh
+++ utils/_banner.sh
@@ -23,16 +23,7 @@ print_banner() {
 
 printf "${CYAN_LIGHT}";
 
-
-printf ${CYAN_LIGHT}" ____   ____  ________  ____    ____   ________    _       ________  ________  _______   \n";  
-printf ${CYAN_LIGHT}"|_  _| |_  _||_   __  ||_   \  /   _| |_   __  |  / \     |  __   _||_   __  ||_   __ \  \n"; 
-printf ${CYAN_LIGHT}"  \ \   / /    | |_ \_|  |   \/   |     | |_ \_| / _ \    |_/  / /    | |_ \_|  | |__) | \n";
-printf ${CYAN_LIGHT}"   \ \ / /     |  _| _   | |\  /| |     |  _|   / ___ \      .'.' _   |  _| _   |  __ /  \n";
-printf ${CYAN_LIGHT}"    \ ' /     _| |__/ | _| |_\/_| |_   _| |_  _/ /   \ \_  _/ /__/ | _| |__/ | _| |  \ \_\n";
-printf ${CYAN_LIGHT}"     \_/     |________||_____||_____| |_____||____| |____||________||________||____| |___|\n";
-                                                                                                                                                         
-  printf "            \033[1;33m        © CANAL VEM FAZER - https://www.youtube.com/channel/UCwFO9ylM7gHxYIXfJqAo1vQ\n";
-  printf "${NC}";
+printf ${CYAN_LIGHT}"INSTALADOR ADAPTADO POR CRISTIANO MEDEIROS  \n";  
 
   printf "\n"
 }
